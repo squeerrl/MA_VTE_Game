e4d58bf979a476b21d00e13902f2b1890ed98d7f
Hannes <hannes.wagner123@gmail.com>
2019-07-10 15:17:49 +0200

    able to start scanning for devices

--- app/src/main/java/com/squeerrl/magdalena/mmm_test/ScreenActivity.java
+++ app/src/main/java/com/squeerrl/magdalena/mmm_test/ScreenActivity.java
@@ -1,13 +1,82 @@
 package com.squeerrl.magdalena.mmm_test;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ScreenActivity extends AppCompatActivity {
+    static final int REQUEST_ENABLE_BT = 1;
+    static final int PERMISSION_REQUEST_COARSE_LOCATION = 2;
+
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen);
+
+        _setupBluetoothConnection();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    _findBluetoothDevices();
+                }
+            }
+        }
+    }
+
+    private void _setupBluetoothConnection() {
+
+        // 1. Get the BluetoothAdapter
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        // If bluetoothAdapter is null, it means that this Android device does not support Bluetooth
+        if (bluetoothAdapter == null) return;
+
+
+        // 2. Enable Bluetooth
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        _requestLocationPermission();
+    }
+
+    private  void _requestLocationPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+        }
+    }
+
+    private void _findBluetoothDevices() {
+        BluetoothLeScanner scanner = bluetoothAdapter.getBluetoothLeScanner();
+
+        ScanCallback mScanCallback = new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+                super.onScanResult(callbackType, result);
+                Log.d("[from _findBluetoothDevices]", result.toString());
+            }
+        };
+
+        Log.d("[from _findBluetoothDevices]", "startScanning");
+        scanner.startScan(mScanCallback);
     }
 }
